bb7598d01c516d9f34c5892de479be9c1b4e0b69
hasuya.hasuya <hasuya.1120@gmail.com>
2020-05-26 22:32:54 +0900

    initialize board

--- src/Page/Game.elm
+++ src/Page/Game.elm
@@ -1,15 +1,58 @@
-module Page.Game exposing (Model, Msg, init, update, view, subscriptions)
+module Page.Game exposing (Model, Msg, init, subscriptions, update, view)
 
 import Html exposing (..)
+import List.Extra
 
 
 type alias Model =
-    {}
+    { board : Board }
+
+
+type alias Board =
+    List Point
+
+
+type alias Point =
+    { x : Int
+    , y : Int
+    , pointStatus : PointStatus
+    }
+
+
+type PointStatus
+    = Wall
+    | Road
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { board = initializeBoard 8 }, Cmd.none )
+
+
+initializeBoard : Int -> Board
+initializeBoard numOfPoint =
+    numOfPoint
+        |> List.range 0
+        |> initializePoints numOfPoint
+
+
+checkPerimeter : Int -> Int -> Int -> PointStatus
+checkPerimeter numOfPoint x y =
+    if x == 0 || y == 0 || x == numOfPoint || y == numOfPoint then
+        Wall
+
+    else
+        Road
+
+
+initializePoint : Int -> Int -> Int -> Point
+initializePoint numOfPoint x y =
+    Point x y (checkPerimeter numOfPoint x y)
+
+
+initializePoints : Int -> List Int -> Board
+initializePoints numOfPoint width =
+    List.Extra.lift2 (initializePoint numOfPoint) width width
 
 
 
